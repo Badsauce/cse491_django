6fe951ba964ca4262bbebdfceaa6a61af51b1452
Nick Rutowski <badsauce@Django.(none)>
2014-04-03 19:32:55 -0400

    initial

---
+++ myproject/imageapp/views.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+
+from myproject.imageapp.models import Document
+from myproject.imageapp.forms import DocumentForm
+
+def list(request):
+    # Handle file upload
+    if request.method == 'POST':
+        form = DocumentForm(request.POST, request.FILES)
+        if form.is_valid():
+            newdoc = Document(docfile = request.FILES['docfile'])
+            newdoc.save()
+
+            # Redirect to the document list after POST
+            return HttpResponseRedirect(reverse('myproject.imageapp.views.list'))
+    else:
+        form = DocumentForm() # A empty, unbound form
+
+    # Load documents for the list page
+    document = Document.objects.order_by('pk').reverse()[0]
+
+    # Render list page with the documents and the form
+    return render_to_response(
+        'imageapp/list.html',
+        {'document': document, 'form': form},
+        context_instance=RequestContext(request)
+    )
